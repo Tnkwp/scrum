d96a0b249ce8f3f2de012bf49a7b17a5ac2f7eaa
Tnkwp <119997936+Tnkwp@users.noreply.github.com>
2025-08-29 10:10:04 +0700

    [projectfrom] update css

--- scrum/src/main.js
+++ scrum/src/main.js
@@ -5,11 +5,12 @@ import router from "./router";
 import { createPinia } from "pinia";
 import GoogleLogin from "vue3-google-login";
 import PrimeVue from 'primevue/config'
-
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App);
 
+app.use(ElementPlus)
 app.use(PrimeVue)
 app.use(createPinia());
 app.use(router);

01a69060d419c90bef1b30430d829becf2473b71
Tnkwp <119997936+Tnkwp@users.noreply.github.com>
2025-09-03 17:47:46 +0700

    update pagination

--- scrum/src/views/history.vue
+++ scrum/src/views/history.vue
@@ -11,19 +11,29 @@
       <div class="flex gap-6 items-end">
         <div>
           <label class="block text-xs text-gray-600 mb-1">From</label>
-          <input
+          <!-- <input
             type="date"
             v-model="fromDate"
             class="border border-gray-300 rounded-lg px-3 py-1.5 w-36 text-sm shadow-sm focus:ring-2 focus:ring-green-400"
-          />
+          /> -->
+          <el-date-picker
+              v-model="fromDate"
+              type="date"
+              placeholder=""
+            />
         </div>
         <div>
           <label class="block text-xs text-gray-600 mb-1">To</label>
-          <input
+          <!-- <input
             type="date"
             v-model="toDate"
             class="border border-gray-300 rounded-lg px-3 py-1.5 w-36 text-sm shadow-sm focus:ring-2 focus:ring-green-400"
-          />
+          /> -->
+          <el-date-picker
+              v-model="toDate"
+              type="date"
+              placeholder=""
+            />
         </div>
       </div>
 
@@ -35,7 +45,7 @@
     <div class="flex justify-center">
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6 w-full max-w-6xl">
         <div
-          v-for="(card, index) in scrumMembers"
+          v-for="(card, index) in paginatedScrumMembers"
           :key="index"
           class="bg-white rounded-2xl shadow-md p-5 flex flex-col gap-3 hover:shadow-xl transition"
         >
@@ -79,7 +89,7 @@
                   <p><strong class="text-gray-900">พรุ่งนี้ทำอะไร:</strong><br />{{ card.tomorrow || "-" }}</p>
                 </template>
 
-                <template v-else-if="card.type === 'friday' || card.type === 'retrospective'">
+                <template v-else-if="card.type === 'weekly' || card.type === 'retrospective'">
                   <p><strong class="">สิ่งที่ทำวันนี้:</strong><br />{{ card.today || "-" }}</p>
                   <p><strong class="">Good:</strong><br />{{ card.good || "-" }}</p>
                   <p><strong class="">Bad:</strong><br />{{ card.bad || "-" }}</p>
@@ -97,11 +107,21 @@
         </div>
       </div>
     </div>
+    <div class="flex justify-center pb-6">
+      <el-pagination
+        v-model:current-page="currentPage"
+        :page-size="pageSize"
+        size="small"
+        background
+        layout="prev, pager, next"
+        :total="scrumMembers.length"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, watch } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import axios from "axios";
 
@@ -114,6 +134,15 @@ const fromDate = ref("");
 const toDate = ref("");
 const allScrumMembers = ref([]);
 const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+
+const currentPage = ref(1);
+const pageSize = ref(6);
+
+const paginatedScrumMembers = computed(() => {
+  const start = (currentPage.value - 1) * pageSize.value;
+  const end = start + pageSize.value;
+  return scrumMembers.value.slice(start, end);
+});
 
 const fetchScrumData = async () => {
   token.value = localStorage.getItem("token");
@@ -146,8 +175,13 @@ const fetchScrumData = async () => {
       created_at: scrum.created_at,
     }));
 
-    allScrumMembers.value = mapped;
-    scrumMembers.value = mapped;
+    const sorted = mapped.sort(
+      // (a, b) => new Date(b.created_at) - new Date(a.created_at)
+      (a, b) => new Date(a.created_at) - new Date(b.created_at)
+    );
+
+    allScrumMembers.value = sorted;
+    scrumMembers.value = sorted;
   } catch (err) {
     console.error("Error fetching scrum data:", err);
   }
@@ -187,5 +221,9 @@ const goBack = () => {
 </script>
 
 <style scoped>
-
+:deep(.el-date-editor.el-input) {
+  width: 9rem; /* w-36 */
+  height: 30px;
+  font-size: 14px;
+}
 </style>
